115c1db361317ed926cbb0f9057b8196bae2ab40
goheesheng <77826239+goheesheng@users.noreply.github.com>
2022-12-24 18:43:01 +0800

    Delete Fallback directory

--- Fallback/interface/ifallback.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IFallback{
-    function owner() external view returns(address);
-    function contribute() external;
-    function withdraw() external;
-}
